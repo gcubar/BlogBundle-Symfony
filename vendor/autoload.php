41ecc40a77f26604353a87ce269393e5b1b9dba6
Gilberto Cuba <gcubar2005@gmail.com>
2013-03-20 15:44:55 -0430

    after update with composer

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitb4b8e31a5aebd45f1d58443bcfa52dac::getLoader();
+return ComposerAutoloaderInitdc25da9724355ee7e64206bd33b3c8a7::getLoader();
